749cf81401d95943c02839c9218b97712874bd06
Zakaria Tobbal <tzakaria@hotmail.fr>
2022-04-14 16:21:09 +0200

    Getting back to old cron system

--- module/Dealabs/newHot.ts
+++ module/Dealabs/newHot.ts
@@ -2,9 +2,6 @@ import puppeteerExtra from 'puppeteer-extra';
 import puppeteerStealth from 'puppeteer-extra-plugin-stealth';
 import cron from 'node-cron';
 import constants from '../../constants';
-const cronitor = require('cronitor')(constants.cronitorKey);
-
-
 
 let hots: {
     title: string; url: string; img: string; upvote: string; price: string; username: string;
@@ -15,168 +12,160 @@ let reqDate: string = "";
 
 (async () => {
     try {
-
-        await cronitor.Monitor.put({
-            type: 'job',
-            key: 'Dealabs.newHots',
-            schedule: '*/2 * * * *', 
-            timezone: 'Europe/Paris'
-        });
-
-        // Preparing puppeteer
-        const proxyServerArgs: string = '--proxy-server=' + constants.proxyServer;
-        puppeteerExtra.use(puppeteerStealth());
-        const browser = await puppeteerExtra.launch({
-            headless: true,
-            args: ['--no-sandbox', proxyServerArgs]
-        });
+        cron.schedule('4 */2 * * * *', async () => {
+            
+            // Preparing puppeteer
+            const proxyServerArgs: string = '--proxy-server='+constants.proxyServer;
+            puppeteerExtra.use(puppeteerStealth());
+            const browser = await puppeteerExtra.launch({
+                headless: true,
+                args: ['--no-sandbox', proxyServerArgs]
+            });
 
 
-        // Opening dealabs hot tab
-        const page = await browser.newPage();
-        await page.authenticate({
-            username: constants.usernameProxy,
-            password: constants.passwordProxy,
-        });
-        const URL = "https://www.dealabs.com/hot";
-        await page.goto(URL, { waitUntil: "networkidle0" });
+            // Opening dealabs hot tab
+            const page = await browser.newPage();
+            await page.authenticate({
+                username: constants.usernameProxy,
+                password: constants.passwordProxy,
+            });
+            const URL = "https://www.dealabs.com/hot";
+            await page.goto(URL, { waitUntil: "networkidle0" });
 
-        // Allow cookies
-        await page.click(
-            "button.flex--grow-1.flex--fromW3-grow-0.width--fromW3-ctrl-m.space--b-2.space--fromW3-b-0"
-        );
+            // Allow cookies
+            await page.click(
+                "button.flex--grow-1.flex--fromW3-grow-0.width--fromW3-ctrl-m.space--b-2.space--fromW3-b-0"
+            );
 
-        const pingMonit = new cronitor.Monitor('Dealabs.newHots');
+            // Retrieving data
+            setTimeout(async () => {
+                try {
+                    // Listing new hot deals
+                    const listDeals = await page.$$("div.threadGrid");
 
-        pingMonit.ping({state: 'run'});
+                    // initiating index for looping list of deals
+                    var limit = 5;
 
-        // Retrieving data
-        setTimeout(async () => {
-            try {
-                // Listing new hot deals
-                const listDeals = await page.$$("div.threadGrid");
+                    hots.length = 0;
 
-                // initiating index for looping list of deals
-                var limit = 5;
+                    // Looping in deals
+                    for (let index = 0; index < limit; index++) {
+                        // Initializing variables
+                        var upvote = "";
+                        var imgDeal = "";
+                        var insertedTime = "";
+                        var expiredTime = "";
+                        var url = "";
+                        var title = "";
+                        var price = "";
+                        var username = "";
 
-                hots.length = 0;
+                        // Check if it is an ad
+                        const pub = await listDeals[index].$("button.cept-newsletter-widget-close")
 
-                // Looping in deals
-                for (let index = 0; index < limit; index++) {
-                    // Initializing variables
-                    var upvote = "";
-                    var imgDeal = "";
-                    var insertedTime = "";
-                    var expiredTime = "";
-                    var url = "";
-                    var title = "";
-                    var price = "";
-                    var username = "";
-
-                    // Check if it is an ad
-                    const pub = await listDeals[index].$("button.cept-newsletter-widget-close")
-
-                    if (pub) {
-                        limit++;
-                        index++;
-                    }
-
-
-                    // Creating boolean for expired or not
-                    var isExpired = false;
-
-                    // Retrieving upvote
-                    const upvoteTag = await listDeals[index].$(
-                        "span.cept-vote-temp.vote-temp.vote-temp--hot"
-                    );
-
-                    // That's the only tag where we know the deal is expired
-                    if (upvoteTag !== null) {
-                        upvote = await page.evaluate((tag) => tag.textContent, upvoteTag);
-                        upvote = upvote.replace(/\s/g, "");
-                    } else {
-                        limit++;
-                        isExpired = true;
-                    }
-
-                    //Condition if isExpired
-                    if (!isExpired) {
-                        // Retrieving image URL
-                        const imgTag = await listDeals[index].$("img.thread-image");
-                        imgDeal = await page.evaluate(
-                            (img) => img.getAttribute("src"),
-                            imgTag
-                        );
-
-                        // Retrieving inserted time
-                        const flameIconTagParent = await listDeals[index].$(
-                            "span.metaRibbon.cept-meta-ribbon.cept-meta-ribbon-hot"
-                        );
-
-                        if (flameIconTagParent) {
-                            const insertedTimeTag = await flameIconTagParent.$('span')
-
-                            if (insertedTimeTag) {
-                                insertedTime = await page.evaluate(
-                                    (tag) => tag.textContent,
-                                    insertedTimeTag
-                                );
-                            }
+                        if (pub) {
+                            limit++;
+                            index++;
                         }
 
 
-                        // Retrieving expired time
-                        const expiresIconTag = await listDeals[index].$("span.metaRibbon.cept-meta-ribbon.cept-meta-ribbon-expires")
-                        if (expiresIconTag) {
-                            const expriesSpanTag = await expiresIconTag.$("span");
+                        // Creating boolean for expired or not
+                        var isExpired = false;
 
-                            if (expriesSpanTag) {
-                                expiredTime = await page.evaluate(
-                                    (tag) => tag.textContent,
-                                    expriesSpanTag
-                                );
-                            }
-                        }
-
-
-                        // Retrieving URL and Title
-                        const titleTag = await listDeals[index].$(
-                            "a.cept-tt.thread-link.linkPlain.thread-title--list"
-                        );
-                        title = await page.evaluate((tag) => tag.textContent, titleTag);
-                        url = await page.evaluate(
-                            (url) => url.getAttribute("href"),
-                            titleTag
+                        // Retrieving upvote
+                        const upvoteTag = await listDeals[index].$(
+                            "span.cept-vote-temp.vote-temp.vote-temp--hot"
                         );
 
-
-                        // Retrieving price
-                        const priceTag = await listDeals[index].$(
-                            "span.thread-price.text--b.cept-tp.size--all-l.size--fromW3-xl"
-                        );
-
-                        if (priceTag) {
-                            price = await page.evaluate((tag) => tag.textContent, priceTag);
+                        // That's the only tag where we know the deal is expired
+                        if (upvoteTag !== null) {
+                            upvote = await page.evaluate((tag) => tag.textContent, upvoteTag);
+                            upvote = upvote.replace(/\s/g, "");
                         } else {
-                            price = 'FREE'
+                            limit++;
+                            isExpired = true;
                         }
 
-                        // Retrieving author username
-                        const userTag = await listDeals[index].$('span.thread-username');
-                        username = await page.evaluate((tag) => tag.textContent, userTag);
-                        username = username.replace(/\s/g, "");
+                        //Condition if isExpired
+                        if (!isExpired) {
+                            // Retrieving image URL
+                            const imgTag = await listDeals[index].$("img.thread-image");
+                            imgDeal = await page.evaluate(
+                                (img) => img.getAttribute("src"),
+                                imgTag
+                            );
 
-                        //Inserting to array of deals
-                        hots.push({
-                            title: title,
-                            url: url,
-                            img: imgDeal,
-                            upvote: upvote,
-                            price: price,
-                            username: username,
-                            insertedTime: insertedTime,
-                            expiredTime: expiredTime
-                        })
+                            // Retrieving inserted time
+                            const flameIconTagParent = await listDeals[index].$(
+                                "span.metaRibbon.cept-meta-ribbon.cept-meta-ribbon-hot"
+                            );
+
+                            if (flameIconTagParent) {
+                                const insertedTimeTag = await flameIconTagParent.$('span')
+
+                                if (insertedTimeTag) {
+                                    insertedTime = await page.evaluate(
+                                        (tag) => tag.textContent,
+                                        insertedTimeTag
+                                    );
+                                }
+                            }
+
+
+                            // Retrieving expired time
+                            const expiresIconTag = await listDeals[index].$("span.metaRibbon.cept-meta-ribbon.cept-meta-ribbon-expires")
+                            if (expiresIconTag) {
+                                const expriesSpanTag = await expiresIconTag.$("span");
+
+                                if (expriesSpanTag) {
+                                    expiredTime = await page.evaluate(
+                                        (tag) => tag.textContent,
+                                        expriesSpanTag
+                                    );
+                                }
+                            }
+
+
+                            // Retrieving URL and Title
+                            const titleTag = await listDeals[index].$(
+                                "a.cept-tt.thread-link.linkPlain.thread-title--list"
+                            );
+                            title = await page.evaluate((tag) => tag.textContent, titleTag);
+                            url = await page.evaluate(
+                                (url) => url.getAttribute("href"),
+                                titleTag
+                            );
+
+
+                            // Retrieving price
+                            const priceTag = await listDeals[index].$(
+                                "span.thread-price.text--b.cept-tp.size--all-l.size--fromW3-xl"
+                            );
+
+                            if (priceTag) {
+                                price = await page.evaluate((tag) => tag.textContent, priceTag);
+                            } else {
+                                price = 'FREE'
+                            }
+
+                            // Retrieving author username
+                            const userTag = await listDeals[index].$('span.thread-username');
+                            username = await page.evaluate((tag) => tag.textContent, userTag);
+                            username = username.replace(/\s/g, "");
+
+                            //Inserting to array of deals
+                            hots.push({
+                                title: title,
+                                url: url,
+                                img: imgDeal,
+                                upvote: upvote,
+                                price: price,
+                                username: username,
+                                insertedTime: insertedTime,
+                                expiredTime: expiredTime
+                            })
+                        }
+
                     }
 
                     //log
@@ -193,24 +182,8 @@ let reqDate: string = "";
                 }finally{
                     await browser.close();
                 }
-
-                //log
-                if (hots.length === 0) {
-                    console.error(new Date().toLocaleString() + " : 0 elements for Dealabs.newHot")
-                }else{
-                    pingMonit.ping({state: 'complete'});
-                }
-
-            } catch (error) {
-                console.error(new Date().toLocaleString() + ' Dealabs.newHot Error: ' + error);
-                pingMonit.ping({state: 'fail'});
-                throw error;
-            } finally {
-                await browser.close();
-            }
-        }, 2000);
-        // cron.schedule('4 */2 * * * *', async () => {
-        // })
+            }, 2000);
+        })
     } catch (error) {
         console.error(new Date().toLocaleString() + ' Dealabs.newHot Error: ' + error);
         throw error;
